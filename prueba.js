4877e8f4df7fb4064d469261d66ffc2ef3a67139
Anbrela <pablonaveiralopez@gmail.com>
2020-12-16 23:00:44 +0100

    Mañana miramos el tema de añadir campos

--- prueba.js
+++ prueba.js
@@ -2,9 +2,9 @@
 
 var request = new XMLHttpRequest()
 var api_key = "d611098e2b30fe621e06d63d02723b1c"
-var with_genres = 35;
+var with_genres = 99;
 var requestURL = "https://api.themoviedb.org/3/discover/movie?api_key="
-  + api_key +"&language=essort_by=popularity.desc&include_adult=false&include_video=false&with_genres=" + with_genres;
+  + api_key +"&language=es&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=" + with_genres;
 
 
 // Open a new connection, using the GET request on the URL endpoint
@@ -19,7 +19,10 @@ request.onload = function () {
 
         if ( i < 5) {
 
-          $('#prueba').append('<img src="https://image.tmdb.org/t/p/w500/' + data.results[i].poster_path + '"/>');
+          //MAÑANA TE ENSEÑO A CREAR UN ELEMENTO EN JS PARA AÑADIR LA PELI Y LA SINOPSIS.
+
+          $('#peli-imagen').append('<img src="https://image.tmdb.org/t/p/w500/' + data.results[i].poster_path + '"/>');
+          $('#sinopsis').append(data.results[i].overview)
           i++;
         }  else {
           return;
