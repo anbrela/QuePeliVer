4437b18de318c99595ea1a331bc069ec759f2ae8
Pablo Naveira López <navloppab@IESPIRINGALLA.AD>
2020-12-18 21:02:55 +0100

    Prueba.

--- js/recomendador.js
+++ js/recomendador.js
@@ -44,17 +44,49 @@ $( document ).ready(function() {
     //FIN MENU
 
 
-var request = new XMLHttpRequest()
-var api_key = "d611098e2b30fe621e06d63d02723b1c"
-var with_genres = 0;
-var requestURL = "https://api.themoviedb.org/3/discover/movie?api_key="
-  + api_key +"&language=es&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=" + with_genres;
+    var request = new XMLHttpRequest()
+    var api_key = "d611098e2b30fe621e06d63d02723b1c"
+    var with_genres = 0;
+    var requestURL = "https://api.themoviedb.org/3/discover/movie?api_key="
+      + api_key +"&language=es&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=" + with_genres;
+
+
+
+
+    //eleccion 1
+
+    const contenedor2 = $('#container');
+    const pelicula = $('#pelicula');
+    const serie = $('#serie');
+    const contenedor1 = $('#eleccion');
+
+    contenedor2.hide();
+
+    if(pelicula.click()) {
+
+      contenedor2.show();
+      contenedor1.hide();
+
+    }
+
+    if(serie.click()) {
+
+      contenedor2.show();
+      contenedor1.hide();
+      var requestURL = "https://api.themoviedb.org/3/discover/tv?api_key="
+        + api_key +"d611098e2b30fe621e06d63d02723b1c&language=es&sort_by=popularity.desc&timezone=Europe&include_null_first_air_dates=false" + with_genres;
+
+
+    }
+
+    else {
+
+    }
+
 
 
 // Open a new connection, using the GET request on the URL endpoint
 request.open('GET', requestURL , true)
-
-
 
 
 request.onload = function () {
