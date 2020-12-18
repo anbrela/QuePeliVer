80191c8300c10f8aecea1840ee933b5cbd311e43
Pablo Naveira López <navloppab@IESPIRINGALLA.AD>
2020-12-18 19:56:29 +0100

    Cambiado el bucle para no mostrar las imágenes rotas.

--- js/recomendador.js
+++ js/recomendador.js
@@ -46,7 +46,7 @@ $( document ).ready(function() {
 
 var request = new XMLHttpRequest()
 var api_key = "d611098e2b30fe621e06d63d02723b1c"
-var with_genres = 99;
+var with_genres = 0;
 var requestURL = "https://api.themoviedb.org/3/discover/movie?api_key="
   + api_key +"&language=es&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=" + with_genres;
 
@@ -56,24 +56,23 @@ request.open('GET', requestURL , true)
 
 
 
+
 request.onload = function () {
   var data = JSON.parse(this.response)
 
-  data.results.forEach((movie, i) => {
-
-        if ( i < 5) {
-
-          //MAÑANA TE ENSEÑO A CREAR UN ELEMENTO EN JS PARA AÑADIR LA PELI Y LA SINOPSIS.
-
-          $('#peli-imagen').append('<img src="https://image.tmdb.org/t/p/w500/' + data.results[i].poster_path + '"/>');
-          $('#sinopsis').append(data.results[i].overview)
-          i++;
-        }  else {
-          return;
-        }
+  var j=0;
+  var total = 0;
+  while (total<4) {
+    if ( data.results[j].poster_path != null ) {
+        $('#peli-imagen').append('<div class="peli-item"><img src="https://image.tmdb.org/t/p/w500/' + data.results[j].poster_path + '"/> </div>');
+        total++;
+    }
+    j++;
+  }
 
 
-});
+
+
 
 
 }
